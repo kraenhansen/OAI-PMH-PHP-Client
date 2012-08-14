b08d4595acf13cdf0af803e59cd1afb75198e25b
Kræn Hansen <post@creen.dk>
2012-08-14 13:45:33 +0200

    Added the last requests.

--- src/oaipmh/OAIPMHClient.php
+++ src/oaipmh/OAIPMHClient.php
@@ -1,6 +1,6 @@
 <?php
 namespace oaipmh;
-use \RuntimeException;
+use \RuntimeException, \SimpleXMLElement;
 class OAIPMHClient {
 	protected $_curlHandle;
 	protected $_baseURL;
@@ -20,8 +20,8 @@ class OAIPMHClient {
 	 * Performs a POST request to the OAI-PMH service.
 	 * @param array[string]string $data Requered arguments.
 	 * @param array[string]null|string $extraArguments
-	 * @throws RuntimeException
-	 * @return SimpleXMLElement
+	 * @throws RuntimeException If something goes wrong.
+	 * @return \SimpleXMLElement
 	 */
 	protected function request($requiredArguments, $optionalArguments = array()) {
 		// Process arguments.
@@ -65,11 +65,11 @@ class OAIPMHClient {
 	 * @see http://www.openarchives.org/OAI/openarchivesprotocol.html#GetRecord
 	 * @param string $identifier Identifier a required argument that specifies the unique identifier of the item in the repository from which the record must be disseminated.
 	 * @param string $metadataPrefix A required argument that specifies the metadataPrefix of the format that should be included in the metadata part of the returned record. A record should only be returned if the format specified by the metadataPrefix can be disseminated from the item identified by the value of the identifier argument. The metadata formats supported by a repository and for a particular record can be retrieved using the ListMetadataFormats request.
-	 * @return SimpleXMLElement
+	 * @return \SimpleXMLElement
 	 */
 	public function GetRecord($identifier, $metadataPrefix) {
 		return $this->request(array(
-			'verb' => 'GetRecord'
+			'verb' => __FUNCTION__
 		), get_defined_vars());
 	}
 	
@@ -77,12 +77,13 @@ class OAIPMHClient {
 	 * This request is used to retrieve information about a repository. Some of the information returned is required as part of the OAI-PMH. Repositories may also employ the Identify verb to return additional descriptive information.
 	 * Error and Exception Conditions:
 	 *  - badArgument - The request includes illegal arguments.
-	 * @return SimpleXMLElement
+	 * @see http://www.openarchives.org/OAI/openarchivesprotocol.html#Itentify
+	 * @return \SimpleXMLElement
 	 */
 	public function Identify() {
 		return $this->request(array(
-			'verb' => 'Identify'
-		));
+			'verb' => __FUNCTION__
+		), get_defined_vars());
 	}
 	
 	/**
@@ -93,36 +94,77 @@ class OAIPMHClient {
 	 *  - cannotDisseminateFormat - The value of the metadataPrefix argument is not supported by the repository.
 	 *  - noRecordsMatch- The combination of the values of the from, until, and set arguments results in an empty list.
 	 *  - noSetHierarchy - The repository does not support sets.
+	 * @see http://www.openarchives.org/OAI/openarchivesprotocol.html#ListIdentifiers
+	 * @param string $metadataPrefix A required argument, which specifies that headers should be returned only if the metadata format matching the supplied metadataPrefix is available or, depending on the repository's support for deletions, has been deleted. The metadata formats supported by a repository and for a particular item can be retrieved using the ListMetadataFormats request.
+	 * @param null|string $resumptionToken An exclusive argument with a value that is the flow control token returned by a previous ListIdentifiers request that issued an incomplete list.
 	 * @param null|string $from An optional argument with a UTCdatetime value, which specifies a lower bound for datestamp-based selective harvesting.
 	 * @param null|string $until An optional argument with a UTCdatetime value, which specifies a upper bound for datestamp-based selective harvesting.
-	 * @param string $metadataPrefix A required argument, which specifies that headers should be returned only if the metadata format matching the supplied metadataPrefix is available or, depending on the repository's support for deletions, has been deleted. The metadata formats supported by a repository and for a particular item can be retrieved using the ListMetadataFormats request.
 	 * @param null|string $set An optional argument with a setSpec value , which specifies set criteria for selective harvesting.
-	 * @param null|string $resumptionToken An exclusive argument with a value that is the flow control token returned by a previous ListIdentifiers request that issued an incomplete list.
-	 * @return SimpleXMLElement
+	 * @return \SimpleXMLElement
 	 */
-	public function ListIdentifiers($from = null, $until = null, $metadataPrefix, $set = null, $resumptionToken = null) {
+	public function ListIdentifiers($metadataPrefix, $resumptionToken = null, $from = null, $until = null, $set = null) {
 		return $this->request(array(
-			'verb' => 'ListIdentifiers'
+			'verb' => __FUNCTION__
 		), get_defined_vars());
 	}
 	
 	/**
-	 * This verb is used to retrieve the metadata formats available from a repository. An optional argument restricts the request to the formats available for a specific item.
-	 * @param unknown_type $identifier
+	 * This request is used to retrieve the metadata formats available from a repository. An optional argument restricts the request to the formats available for a specific item.
+	 * Error and Exception Conditions:
+	 *  - badArgument - The request includes illegal arguments or is missing required arguments.
+	 *  - idDoesNotExist - The value of the identifier argument is unknown or illegal in this repository.
+	 *  - noMetadataFormats - There are no metadata formats available for the specified item.
+	 * @see http://www.openarchives.org/OAI/openarchivesprotocol.html#ListMetadataFormats
+	 * @param null|string $identifier An optional argument that specifies the unique identifier of the item for which available metadata formats are being requested. If this argument is omitted, then the response includes all metadata formats supported by this repository. Note that the fact that a metadata format is supported by a repository does not mean that it can be disseminated from all items in the repository.
+	 * @return \SimpleXMLElement
 	 */
 	public function ListMetadataFormats($identifier = null) {
 		return $this->request(array(
-			'verb' => 'ListIdentifiers'
+			'verb' => __FUNCTION__
+		), get_defined_vars());
+	}
+	
+	/**
+	 * This request is an abbreviated form of ListRecords, retrieving only headers rather than records. Optional arguments permit selective harvesting of headers based on set membership and/or datestamp. Depending on the repository's support for deletions, a returned header may have a status attribute of "deleted" if a record matching the arguments specified in the request has been deleted.
+	 * Error and Exception Conditions
+	 *  - badArgument - The request includes illegal arguments or is missing required arguments.
+	 *  - badResumptionToken - The value of the resumptionToken argument is invalid or expired.
+	 *  - cannotDisseminateFormat - The value of the metadataPrefix argument is not supported by the repository.
+	 *  - noRecordsMatch- The combination of the values of the from, until, and set arguments results in an empty list.
+	 *  - noSetHierarchy - The repository does not support sets.
+	 * @see http://www.openarchives.org/OAI/openarchivesprotocol.html#ListRecords
+	 * @param string $metadataPrefix
+	 * @param null|string $resumptionToken
+	 * @param null|string $from
+	 * @param null|string $until
+	 * @param null|string $set
+	 * @return \SimpleXMLElement
+	 */
+	public function ListRecords($metadataPrefix, $resumptionToken = null, $from = null, $until = null, $set = null) {
+		return $this->request(array(
+			'verb' => __FUNCTION__
+		), get_defined_vars());
+	}
+	
+	/**
+	 * This request is used to retrieve the set structure of a repository, useful for selective harvesting.
+	 * Error and Exception Conditions
+	 *  - badArgument - The request includes illegal arguments or is missing required arguments.
+	 *  - badResumptionToken - The value of the resumptionToken argument is invalid or expired.
+	 *  - noSetHierarchy - The repository does not support sets.
+	 * @see http://www.openarchives.org/OAI/openarchivesprotocol.html#ListSets
+	 * @param null|string $resumptionToken An exclusive argument with a value that is the flow control token returned by a previous ListSets request that issued an incomplete list.
+	 * @return \SimpleXMLElement
+	 */
+	public function ListSets($resumptionToken = null) {
+		return $this->request(array(
+			'verb' => __FUNCTION__
 		), get_defined_vars());
 	}
 	
 	protected function sanityCheck() {
 		$response = $this->Identify();
 		printf("Repository is '%s' (OAI-PMH version %s)\n", $response->Identify->repositoryName, $response->Identify->protocolVersion);
-	}
-	
-	public function test() {
-		$response = $this->ListIdentifiers(null, null, "test");
 	}
 	
 }
